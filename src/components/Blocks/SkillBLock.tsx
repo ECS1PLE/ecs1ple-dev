5eca00ce45e7dddf7d4c64711b5349ab7fab5abc
ECS1PLE <artyom.vavilov78@gmail.com>
2025-12-24 23:31:50 +0300

    Fix skills

--- src/components/Blocks/SkillBLock.tsx
+++ src/components/Blocks/SkillBLock.tsx
@@ -7,7 +7,7 @@ interface SkillBlockProps {
 const SkillBlock = ({ icon, text, className = "" }: SkillBlockProps) => {
   return (
     <div
-      className={`flex items-center flex-col w-[198px] h-[164px] bg-[#0d0d0d] -z-1 rounded-[20px] p-[16px] ${className}`}
+      className={`flex items-center flex-col w-[228px] h-[164px] bg-[#0d0d0d] -z-1 rounded-[20px] p-[16px] ${className}`}
     >
       {icon}
       <span className="text-white text-[24px] mt-auto">{text}</span>
